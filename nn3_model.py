74333cd4e46385c61d73c3113340605dbea42b72
Fat Tang <136308722+FatTang2012@users.noreply.github.com>
2024-05-06 23:11:14 +0700

    Final

--- nn3_model.py
+++ nn3_model.py
@@ -8,7 +8,6 @@ import os
 
 class NN:
     def __init__(self, X_train, Y_train):
-        # Kaiming He initialization for the weights
         self.W1 = np.random.randn(256, 784) * np.sqrt(2/784) # (256, 784)
         self.b1 = np.zeros((256, 1)) # (256, 1)
         self.W2 = np.random.randn(10, 256) * np.sqrt(2/256) # (10, 256)
@@ -55,28 +54,25 @@ class NN:
 
     def predict(self, X):
         self.forward_prop(X)
-        return np.argmax(self.A2, axis=0) #take the index of the highest value in each column
+        return np.argmax(self.A2, axis=0) 
 
     def accuracy(self, prediction, y):
-        return np.sum(prediction == y) / y.size #sum of correct predictions / total number of predictions
+        return np.sum(prediction == y) / y.size 
 
     def random_mini_batches(self, X, y, mini_batch_size=64, seed=0):
         np.random.seed(seed)
         m = X.shape[1]
         mini_batches = []
     
-        # step 1: shuffle (X, y)
         permutation = list(np.random.permutation(m)) # [0, 1, 2, ..., data_lenght-1] in random order
         X_shuffled = X[:, permutation]
         y_shuffled = y[permutation]
     
-        # step 2: partition (shuffle_X, shuffle_y), handle the end case
         n_batches = math.floor(m / mini_batch_size)
         for k in range(n_batches):
             mini_batch_X = X_shuffled[:, k*mini_batch_size:(k+1)*mini_batch_size]
             mini_batch_y = y_shuffled[k*mini_batch_size:(k+1)*mini_batch_size]
             mini_batches.append((mini_batch_X, mini_batch_y))
-        # Handling the end case (last mini-batch < mini_batch_size)
         if m % mini_batch_size != 0:
             mini_batch_X = X_shuffled[:, int(m/mini_batch_size)*mini_batch_size:]
             mini_batch_y = y_shuffled[int(m/mini_batch_size)*mini_batch_size:]
@@ -84,10 +80,7 @@ class NN:
         return mini_batches 
         
     def save_model(self, file_path):
-    # Create the directory if it doesn't exist
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    
-    # Save the model
         np.savez(file_path, W1=self.W1, b1=self.b1, W2=self.W2, b2=self.b2)
         
     def load_model(self, file_path):
@@ -96,12 +89,6 @@ class NN:
             self.b1 = data['b1']
             self.W2 = data['W2']
             self.b2 = data['b2']
-
-    def print_weights_and_biases(self):
-        print("W1:", self.W1)
-        print("b1:", self.b1)
-        print("W2:", self.W2)
-        print("b2:", self.b2)
 
     def fit(self, X, y, mini_batch_size, lr, n_epochs, save_path):
         seed = 10
@@ -121,110 +108,19 @@ class NN:
         preds = self.predict(X)
         print('\nModel accuracy on training data:', self.accuracy(preds, y))
 
-        # Save the model
         self.save_model(save_path)
 
-    def test_prediction(self, index,X,Y):
-        current_image = X[:, index]
-        prediction = self.predict(current_image)
-        label = Y[index]
-        print("Prediction: ", prediction)
-        print("Label: ", label)
-        current_image = current_image.reshape((28, 28)) * 255
-        plt.gray()
-        plt.imshow(current_image, interpolation='nearest')
-        plt.show()
         
-image_path = ".png"   
-# Load the MNIST dataset
+image_path = "D:\\Document move here\\Learning\\Hoc ki\\N2\\HK2 N2\\mang than kinh\\full set number\\ve.png"   
+
 def train_model():
-    # Load data
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-    # Reshape and normalize the input data
     X_train = X_train.reshape(X_train.shape[0], -1).T / 255.
 
-    # Create an instance of the NN class and train the model
     nn = NN(X_train, y_train)
-    nn.fit(X_train, y_train, 512, 0.01, 200, 'save_model/my_model')
+    nn.fit(X_train, y_train, 512, 0.01, 400, 'save_model/my_model')
 
 if __name__ == "__main__":
     train_model()
 
-#def one_hot(y):
-#    n_classes = len(np.unique(y_train))
-#    y_oh = np.eye(n_classes)
-#    return y_oh.T
-#a=one_hot(y_train)[[5]]
-#print(a)
-#
-
-#m=X_train.shape[1]
-#permutation = list(np.random.permutation(m)) 
-#X_shuffled = X_train[:, permutation]
-#y_shuffled = y_train[permutation]
-#mini_batch_size=3
-#k=1
-#mini_batch=[]
-#mini_batch_X = X_shuffled[:, k*mini_batch_size:(k+1)*mini_batch_size]
-#mini_batch_Y = y_shuffled[k*mini_batch_size:(k+1)*mini_batch_size]
-#mini_batch.append((mini_batch_X, mini_batch_Y))
-
-#index=20
-#img=X_train[:,index].reshape(28,28)*255
-#plt.gray()
-#plt.imshow(img, interpolation='nearest')
-#print(y_train[index])
-#plt.show()
-
-# Create an instance of the NN class and train the model
-#nn = NN(X_train, y_train)
-#nn.fit(X_train, y_train, 512, 0.01, 10, 'save_model/my_model')
-
-#def digit_recognizer(image_path):
-    # Đọc ảnh từ đường dẫn
-   # image = plt.imread(image_path)
-    # Chuyển ảnh thành dạng xám
-   # gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # Resize ảnh thành kích thước (28, 28)
-  #  resized_image = cv2.resize(gray_image, (300, 300))
-    # Chuẩn hóa ảnh và chuyển thành vectơ cột
-   # normalized_image = resized_image.reshape(-1, 1).astype('float32') / 255.
-  #  # Dự đoán chữ số từ ảnh
- #   prediction = nn.predict(normalized_image)
-#    return prediction
-
-    
-#def show_popup(image_path):
-    # Đọc ảnh từ đường dẫn
-  #  image = plt.imread(image_path)
-    # Chuyển ảnh thành dạng xám
-  #  gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    # Reshape ảnh thành kích thước (784,)
-   # reshaped_image = gray_image.reshape(1, -1).T  # Đảo ngược chiều của ảnh và chuyển thành vectơ cột
-    # Chuẩn hóa ảnh
-    #normalized_image = reshaped_image.astype('float32') / 255.
-    #print("Kích thước của normalized_image:", normalized_image.shape)  # In ra kích thước của normalized_image
-    # Dự đoán chữ số từ ảnh
-   # predicted_digit = digit_recognizer(image_path)
-    # Tạo cửa sổ popup
-   # root = Tk()
-    #root.title("Kết quả dự đoán")
-    
-    # Hiển thị ảnh trong popup
-   # img = PhotoImage(file=image_path)
-  #  Label(root, image=img).pack(side=TOP)
-
-    # Hiển thị kết quả dự đoán
- #   Label(root, text="Kết quả dự đoán: {}".format(predicted_digit)).pack(side=TOP)
-
-#    root.mainloop()
-
-#show_popup(image_path)
-#iface = gr.Interface(
-#    fn=digit_recognizer,
-#    inputs=gr.Image(shape=(28,28), image_mode='L', invert_colors=True, source="canvas"),
-#    # outputs=[gr.Image(shape=(28,28)), gr.outputs.Textbox()]
-#    outputs=gr.outputs.Textbox(),
-#)
-#iface.launch()
